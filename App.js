8e9b6fef038649d6dfe09e4b9df3f92fccce43ee
Ndimande <mkatroso.deep@gmail.com>
2023-07-26 23:57:34 +0200

    Meme Generator

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header"
+import Form from "./components/Form"
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Form/>
+        </div>
+    )
+}
